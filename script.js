ff74554711dc4aeb69413da06dc10ea53093cecc
YaBoiBull <166940153+YaBoiBull@users.noreply.github.com>
2024-05-29 21:36:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,13 @@ function createFlower() {
   const yPosition = Math.random() * window.innerHeight;
   flower.style.left = `${xPosition}px`;
   flower.style.top = `${yPosition}px`;
-  flower.style.animation = 'float 5s infinite, fadeIn 2s forwards';
 
   document.body.appendChild(flower);
 
   // Fjern blomsten efter animationen er fuldført for at forhindre ophobning i DOM'en
   setTimeout(() => {
     flower.remove();
-  }, 5000); // Samlet varighed af fadeIn + holdtid
+  }, 7000); // Samlet varighed af float animation + holdtid
 }
 
 // Opret en ny blomst hvert 4. sekund
